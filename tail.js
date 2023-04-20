90a1529db9641963fad481e3dfc96d9287bea146
Ryan Stewart <ryanstew17@gmail.com>
2023-04-20 10:40:33 -0300

    comparing ends of arrays

---
+++ tail.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😀😀😀Assertion Passed: "  + `${actual}` + " === " + `${expected}`);
+  } else {
+    (actual !== expected);
+    console.log("🤬🤬🤬Assertion Failed: " + `${actual}` + " !== " + `${expected}`);
+  }
+};
+
+const tail = function(array) {
+  return array.slice(1);
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
