7d4504c6e2616ddec60eb05328f08ccb635ff440
Ryan Stewart <ryanstew17@gmail.com>
2023-04-22 12:00:13 -0300

    comparing arrays countued

---
+++ assertArraysEqual.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😀😀😀Assertion Passed: "  + `${actual}` + " === " + `${expected}`);
+  } else {
+    (actual !== expected);
+    console.log("🤬🤬🤬Assertion Failed: " + `${actual}` + " !== " + `${expected}`);
+  }
+};
+let eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1.length === array2.length) {
+      if (array1[i] === array2[i]) {
+        return true;
+      } else return false;
+    }
+  }
+  return false;
+};
+
+let assertArrayEqual = function(array1,array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${array1} !== ${array2}`);
+  }
+};
+
+assertArrayEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertArrayEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertArrayEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertArrayEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
