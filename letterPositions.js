2940634bd51d7519726b937ab56c3b7fc01b478f
Ryan Stewart <ryanstew17@gmail.com>
2023-04-25 19:57:29 -0300

    postitions of letters in a string

---
+++ letterPositions.js
@@ -0,0 +1,32 @@
+let eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1.length === array2.length) {
+      if (array1[i] === array2[i]) {
+        return true;
+      } else return false;
+    }
+  }
+  return false;
+};
+
+let assertArrayEqual = function(array1,array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${array1} !== ${array2}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i)
+    } else {
+      results[sentence[i]] = [i];
+    }
+
+  }
+  return results;
+};
+assertArrayEqual(letterPositions("hello").e, [1]);
