9f9729be3f46cf78d67a32d96cd280cfbae3ef19
Ryan Stewart <ryanstew17@gmail.com>
2023-04-25 21:40:07 -0300

    linted

--- letterPositions.js
+++ letterPositions.js
@@ -21,7 +21,7 @@ const letterPositions = function(sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++) {
     if (results[sentence[i]]) {
-      results[sentence[i]].push(i)
+      results[sentence[i]].push(i);
     } else {
       results[sentence[i]] = [i];
     }
