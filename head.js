58ba64fd014afe4e499adccffcc5416d552b8463
Ryan Stewart <ryanstew17@gmail.com>
2023-05-08 19:14:54 -0300

    drying up code by putting test files in folder

--- head.js
+++ head.js
@@ -1,16 +1,7 @@
-// Use to write test cases for various scenarios
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("😀😀😀Assertion Passed: "  + `${actual}` + " === " + `${expected}`);
-  } else {
-    (actual !== expected);
-    console.log("🤬🤬🤬Assertion Failed: " + `${actual}` + " !== " + `${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 const head = function(array) {
   return array[0];
 };
 
-(assertEqual(head([5,6,7]), 5));
-(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+module.exports = head;
 
