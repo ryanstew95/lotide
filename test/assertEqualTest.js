09e7bbb21b1dee8702e38dcf6861867d01cd79f5
Ryan Stewart <ryanstew17@gmail.com>
2023-05-08 19:17:39 -0300

    test code for head and assert equal

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+(assertEqual("Lighthouse Labs", "Bootcamp"));
+(assertEqual(1, 1));
+(assertEqual("ryan", "ryan"));
+(assertEqual("pie", "apple"));
+(assertEqual(4, 4));
+(assertEqual(4, 4.4));
