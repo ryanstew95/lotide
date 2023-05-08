d1185e8c925478a661a40a448d007e049bade083
Ryan Stewart <ryanstew17@gmail.com>
2023-05-08 19:28:19 -0300

    refactor tail

--- tail.js
+++ tail.js
@@ -1,21 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("😀😀😀Assertion Passed: "  + `${actual}` + " === " + `${expected}`);
-  } else {
-    (actual !== expected);
-    console.log("🤬🤬🤬Assertion Failed: " + `${actual}` + " !== " + `${expected}`);
-  }
-};
-
+const assertEqual = require('./assertEqual');
 const tail = function(array) {
   return array.slice(1);
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+module.exports = tail;
