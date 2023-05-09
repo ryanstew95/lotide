58474d7edbd004a6bc2d098e9ccd1eaf955a8fba
Ryan Stewart <ryanstew17@gmail.com>
2023-05-09 16:55:56 -0300

    master file with all the functions

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const assertArrayEqual = require('./assertArraysEqual');
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  eqArrays: eqArrays,
+  assertArrayEqual: assertArrayEqual
+
+};
+
