908a384bc66c424c0e2e3e4cdb2cefac519474cd
Ryan Stewart <ryanstew17@gmail.com>
2023-05-09 17:14:32 -0300

    implement object shorthand

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const middle = require('./middle');
 const eqArrays = require('./eqArrays');
 const assertArrayEqual = require('./assertArraysEqual');
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  eqArrays: eqArrays,
-  assertArrayEqual: assertArrayEqual
+  head,
+  tail,
+  middle,
+  eqArrays,
+  assertArrayEqual
 
 };
 
