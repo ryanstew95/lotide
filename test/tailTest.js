a2f953080a640f2cf9f901c3acc26a7d128b18b6
Ryan Stewart <ryanstew17@gmail.com>
2023-05-09 12:34:27 -0300

    test with chai and mocha

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,16 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+
+
+describe("#tail", () => {
+  it("returns 3 for [2, 3]", () => {
+    assert.deepEqual(tail([2, 3]), ([3]));
+  });
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), ([2, 3]));
+  });
+});
+
+
