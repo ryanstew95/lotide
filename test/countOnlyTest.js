877a20c61e52752d192ffd163c987428930d6456
Ryan Stewart <ryanstew17@gmail.com>
2023-05-09 21:28:48 -0300

    count only test with chai and mocha

---
+++ test/countOnlyTest.js
@@ -0,0 +1,21 @@
+const assert = require('chai').assert;
+// const assertEqual = require('../assertEqual');
+const countOnly = require('../countOnly');
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+describe("#countOnly", () => {
+  it("returns 1 for result1[Jason];", () => {
+    assert.deepEqual(result1["Jason"], 1);
+  });
+});
