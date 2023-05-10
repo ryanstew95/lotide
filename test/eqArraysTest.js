f79f5641c08f66fd903f1b0d9b3fb799fc283665
Ryan Stewart <ryanstew17@gmail.com>
2023-05-09 22:44:43 -0300

    testing with mocha and chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,8 +1,13 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+// const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+describe("#eqArrays", () => {
+  it("returns true for [1, 2, 3]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it("returns true for ['1', '2', '3']", () => {
+    assert.strictEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
+  });
+});
