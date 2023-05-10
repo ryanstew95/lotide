437cc4b3eed0d6fe5dacb428325e815ba6761ec0
Ryan Stewart <ryanstew17@gmail.com>
2023-05-09 22:33:50 -0300

    eqObjects test

---
+++ test/eqObjectsTest.js
@@ -0,0 +1,17 @@
+const assert = require('chai').assert;
+// const eqArrays = require('../eqArrays');
+// const assertEqual = require('../assertEqual');
+const eqObjects = require('../eqObjects');
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+
+describe("#eqObjects", () => {
+  it("returns true for (shirtObject , anotherShirtObject)", () => {
+    assert.deepEqual(eqObjects(shirtObject , anotherShirtObject), true);
+  });
+  it("returns false for (shirtObject , longSleeveShirtObject)", () => {
+    assert.deepEqual(eqObjects(shirtObject , longSleeveShirtObject), false);
+  });
+});
