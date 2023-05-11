b2ad36c40520649b8c4a4531c49eb2fc1f392942
Ryan Stewart <ryanstew17@gmail.com>
2023-05-11 11:09:59 -0300

    cleaning up code

--- takeUntil.js
+++ takeUntil.js
@@ -1,21 +1,5 @@
-let eqArrays = function(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1.length === array2.length) {
-      if (array1[i] === array2[i]) {
-        return true;
-      } else return false;
-    }
-  }
-  return false;
-};
-
-const assertArrayEqual = function(array1,array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion failed: ${array1} !== ${array2}`);
-  }
-};
+// const eqArrays = require('./eqArrays');
+// const assertArrayEqual = require('./assertArraysEqual');
 
 const takeUntil = function(array, callback) {
   const results = [];
@@ -26,21 +10,4 @@ const takeUntil = function(array, callback) {
     results.push(array[i]);
   }
 };
-
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-
-const data3 = ["Testing", "one", "two", "three", ",", "Testing", "one", "two", "three", "!"];
-const results3 = takeUntil(data3, x => x === ',');
-console.log(results3);
-
-assertArrayEqual(results1, data1.slice(0, 5));
-assertArrayEqual(results2, data2.slice(0, 4));
-assertArrayEqual(results3, data3.slice(0, 4));
+module.exports = takeUntil;
--- without.js
+++ without.js
@@ -1,28 +1,3 @@
-let eqArrays = function(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1.length === array2.length) {
-      if (array1[i] === array2[i]) {
-        return true;
-      } else return false;
-    }
-  }
-  return false;
-};
-
-let assertArrayEqual = function(array1,array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion failed: ${array1} !== ${array2}`);
-  }
-};
-console.log("assertArrayEqual:");
-assertArrayEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArrayEqual([1, 2, 3], [3, 2, 1]); // => false
-
-assertArrayEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArrayEqual(["1", "2", "3"], ["1", "2", 3]); // => false
-
 let without = function(array1, array2) {
   for (let i = 0; i < array2.length; i++) {
     const index = array1.indexOf(array2[i]);
@@ -33,7 +8,5 @@ let without = function(array1, array2) {
   return array1;
 };
 
-console.log("without:");
 
-assertArrayEqual(without([1, 2, 3], [1]), [2, 3]); // => [2, 3]
-assertArrayEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]); // => ["1", "2"]
+module.exports = without;
