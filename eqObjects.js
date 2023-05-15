30486dc1a4e24a2c669820b2f8071d603a42a3ac
Ryan Stewart <ryanstew17@gmail.com>
2023-05-15 20:56:13 -0300

    cleaned up code

--- eqObjects.js
+++ eqObjects.js
@@ -1,11 +1,19 @@
 const eqArrays = require('./eqArrays');
+
 const eqObjects = function(object1, object2) {
+
   let result = false;
-  if (Object.keys(object1).length === Object.keys(object2).length) {
-    Object.keys(object1).forEach(element => {
-      if (Object.keys(object2).includes(element) && object1[element] === object2[element]) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  const arrIsArr = Array.isArray;
+
+  if (keys1.length === keys2.length) {
+
+    keys1.forEach(element => {
+      if (keys2.includes(element) && object1[element] === object2[element]) {
         result = true;
-      } else if (Array.isArray(object1[element]) === Array.isArray(object2[element])) {
+
+      } else if (arrIsArr(object1[element]) === arrIsArr(object2[element])) {
         result = eqArrays(object1[element], object2[element]);
       } else {
         result = false;
@@ -14,5 +22,6 @@ const eqObjects = function(object1, object2) {
   }
   return result;
 };
+
 module.exports = eqObjects;
 
