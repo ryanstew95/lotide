ff25ae06eb4dc8863ab43da100a83883518c9f27
Ryan Stewart <ryanstew17@gmail.com>
2023-05-15 20:45:09 -0300

    cleaning up code

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,17 @@
-// const assertEqual = require('./assertEqual');
 let eqArrays = function(array1, array2) {
+
   for (let i = 0; i < array1.length; i++) {
+
+    if (array1.length !== array2.length) {
+      return false;
+    }
     if (array1.length === array2.length) {
       if (array1[i] === array2[i]) {
         return true;
-      } else return false;
+      }
+      return false;
     }
   }
-  return true;
 };
 
 module.exports = eqArrays;
