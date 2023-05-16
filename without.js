55315c177d6df78e09df1519259a153e18a62d5d
Ryan Stewart <ryanstew17@gmail.com>
2023-05-15 21:07:01 -0300

    cleaning up code

--- without.js
+++ without.js
@@ -1,8 +1,8 @@
 let without = function(array1, array2) {
   for (let i = 0; i < array2.length; i++) {
     const index = array1.indexOf(array2[i]);
-    if (index > -1) { // only splice array when item is found
-      array1.splice(index, 1); // 2nd parameter means remove one item only
+    if (index > -1) {
+      array1.splice(index, 1);
     }
   }
   return array1;
